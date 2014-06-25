ef06e01ac1594c2ac895793e5b28a67679401168
makotow <makoto.wata@gmail.com>
2014-06-25 23:50:07 +0900

    Go lang study: Basic syntax

---
+++ examples/pointer.go
@@ -0,0 +1,28 @@
+package main
+
+//https://gobyexample.com/pointers
+
+import "fmt"
+
+func zeroval(ival int) {
+  ival = 0
+}
+
+func zeroptr(iptr *int) {
+  // * means dereferences the pointer
+  *iptr = 0
+}
+
+func main() {
+  i := 1
+  fmt.Println("Initial:", i)
+
+  zeroval(i)
+  fmt.Println("zeroval:", i)
+
+  // &i syntax gives the memory address of i
+  zeroptr(&i)
+  fmt.Println("zeroptr:", i)
+
+  fmt.Println("pointer:", &i)
+}
